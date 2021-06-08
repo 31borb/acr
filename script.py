f1cac18cae9ff2cac24a8a465436d9e993c3988d
31borb <85390789+31borb@users.noreply.github.com>
2021-06-08 12:09:42 +0100

    Update script.py
    probs doesnt work

--- script.py
+++ script.py
@@ -4,32 +4,28 @@ import appJar
 import mouse
 import keyboard
 
-app = appJar.gui("acr ")
-app.setSize("300x300")
-app.setSticky("new")
-app.setSticky("e")
-app.addLabel("Hold to", row=0)
-app.radioButton("onoff", "off", row=1, column=0)
-app.radioButton("onoff", "on", row=1, column=2)
-
 def clicker():
-    onoff = app.radioButton("onoff")
-    if(onoff == "on"):
+        toClick = app.radioButton("toClick")
+        if(toClick == "Left Click"):
+            toClick = "left"
+        else:
+            toClick = "right"
         while True:
-            while mouse.is_pressed(button="left") == True:
-                pyautogui.click(button="left", interval=0.1)
-            if(onoff == "off"):
-                while True:
-                    while mouse.is_pressed(button="right") == True:
-                        pyautogui.click(button="right", interval=0.1)
+            while mouse.is_pressed(button=toClick) == True:
+                pyautogui.click(button=toClick, interval=0.1) 
 
-for i in range(6):
+for i in range(3):
     thread = threading.Thread(target=clicker)
     thread.start()
 
-def checkStop():
-    if keyboard.is_pressed("ESC"):
-        return app.yesNoBox("Confirm Exit", "Are you sure you want to exit?")
-    app.setStopFunction(checkStop)
+app = appJar.gui("acr")
+app.setSize("300x300")
+app.setSticky("new")
+app.setSticky("e")
+app.addLabel("Hold to start", row=0)
+app.setSticky("e")
+app.radioButton("toClick", "Right Click", row=1)
+app.setSticky("w")
+app.radioButton("toClick", "Left Click", row=1)
 
 app.go()
